715b8f5bb5190d645020a054d3401f61eaeeb369
philinvested <philinvested@gmail.com>
2017-12-12 16:44:43 -0500

    fixed indentation issue and working on getting player 2 to appear

--- app.js
+++ app.js
@@ -1,51 +1,47 @@
-
-let game_on = true;
+// let game_on = true;
 let table = $("table tr");
-// player 1 Turn Mechanics
-let player1 = prompt("Player One: Enter your name, you will be Blue");
+
+// player set up
+let player1 = prompt("Player One: Input your name. Your assigned disc color will be Black");
 let player1Color = "rgb(0,0,0)";
+let player2 = prompt("Player Two: Input your name. Your assigned disc color will be will be Red");
+let player2Color = "rgb(237, 45, 73)";
+
 let activePlayer = 1;
 let activeName = player1;
 let activeColor = player1Color;
 
 $(document).ready(function() {
-    $('h3').text(
-      player1 + ": it is your move to pick a column to drop your black disc"
-    );
-    $('.board button').on('click', function() {
-      let col = $(this)
-        .closest('td')
+    $("h3").text(player1 + ": it is your move to choose a column to drop your black discs");
+    $(".board button").on("click", function() {
+        let col = $(this)
+        .closest("td")
         .index();
-      let bottomOpen = reviewBottom(col);
-      switchColor(bottomOpen, col, activeColor);
-    });
+        let bottomOpen = reviewBottom(col);
+        switchColor(bottomOpen, col, activeColor);
 
-    if(currentPlayer === 1){
-        activeName = player1;
-        $('h3').text(activeName+": it is your turn, please pick a column to drop your black discs.")
-        activeColor = player1Color;
-      } else {
-        currentName = player2;
-        $('h3').text(currentName+": it is your turn, please pick a column to drop your red chip.");
-        currentColor = player2Color;
-      }
-  });
+        activePlayer = activePlayer * -1;
 
-// Player 2 Turn Mechanics
-let player2 = prompt("Player Two: Enter your name, you will be Red");
-let player2Color = "rgb(237, 45, 73)";
-
-activePlayer = activePlayer * -1;
-
+        if (currentPlayer === 1) {
+            activeName = player1;
+            $("h3").text(activeName +": it is your move to choose a column to drop your black discs.");
+            activeColor = player1Color;
+        } else {
+            activeName = player2;
+            $("h3").text(activeName +": it is your move to choose a column to drop your red discs.");
+            activeColor = player2Color;
+        }
+    })
+});
 
 let showWin = (rowNum, colNum) => {
-  console.log("You won starting at this row,col");
-  console.log(rowNum);
-  console.log(colNum);
-};
+    console.log("You won starting at this row,col");
+    console.log(rowNum);
+    console.log(colNum);
+}
 
 function switchColor(rowIndex, colIndex, color) {
-  return table
+    return table
     .eq(rowIndex)
     .find("td")
     .eq(colIndex)
@@ -54,7 +50,7 @@ function switchColor(rowIndex, colIndex, color) {
 }
 
 function returnColor(rowIndex, colIndex) {
-  return table
+    return table
     .eq(rowIndex)
     .find("td")
     .eq(colIndex)
@@ -63,21 +59,31 @@ function returnColor(rowIndex, colIndex) {
 }
 
 function reviewBottom(colIndex) {
-  let showColor = returnColor(5, colIndex);
-  for (let row = 5; row >= 0; row--) {
-    showColor = returnColor(row, colIndex);
-    if (showColor === 'rgb(255, 255, 255)') {
-      return row;
+    let showColor = returnColor (5, colIndex);
+    for (let row = 5; row >= 0; row--) {
+        showColor = returnColor(row, colIndex);
+        if (showColor === "rgb(255, 255, 255)") {
+            return row;
+        }
     }
-  }
 }
 
 function reviewColorMatch(one, two, three, four) {
-  return (
+    return (
     one === two &&
     one === three &&
     one === four &&
     one !== "rgb(255, 255, 255)" &&
     one !== undefined
-  );
+    );
+}
+
+function reviewColorMatch(one, two, three, four) {
+    return (
+    one === two &&
+    one === three &&
+    one === four &&
+    one !== "rgb(255, 255, 255)" &&
+    one !== undefined
+    );
 }
