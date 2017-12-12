c3e0b39771a6f64d6917482fee0aea211a394962
philinvested <philinvested@gmail.com>
2017-12-12 15:53:52 -0500

    added logic for player 1 or player 2 turns

--- app.js
+++ app.js
@@ -3,28 +3,39 @@ let game_on = true;
 let table = $("table tr");
 // player 1 Turn Mechanics
 let player1 = prompt("Player One: Enter your name, you will be Blue");
-let player1Color = 'rgb(0,0,0)';
+let player1Color = "rgb(0,0,0)";
 let activePlayer = 1;
 let activeName = player1;
 let activeColor = player1Color;
 
 $(document).ready(function() {
-    $("h3").text(
+    $('h3').text(
       player1 + ": it is your move to pick a column to drop your black disc"
     );
-    $(".board button").on("click", function() {
+    $('.board button').on('click', function() {
       let col = $(this)
-        .closest("td")
+        .closest('td')
         .index();
       let bottomOpen = reviewBottom(col);
       switchColor(bottomOpen, col, activeColor);
     });
+
+    if(currentPlayer === 1){
+        activeName = player1;
+        $('h3').text(activeName+": it is your turn, please pick a column to drop your black discs.")
+        activeColor = player1Color;
+      } else {
+        currentName = player2;
+        $('h3').text(currentName+": it is your turn, please pick a column to drop your red chip.");
+        currentColor = player2Color;
+      }
   });
 
 // Player 2 Turn Mechanics
-// let player2 = prompt("Player Two: Enter your name, you will be Red");
+let player2 = prompt("Player Two: Enter your name, you will be Red");
 let player2Color = "rgb(237, 45, 73)";
 
+activePlayer = activePlayer * -1;
 
 
 let showWin = (rowNum, colNum) => {
