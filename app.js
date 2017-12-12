b74420c1dbcc1298bdd8e629a6864c03d2484eaa
philinvested <philinvested@gmail.com>
2017-12-12 15:21:33 -0500

    Allocated player 1 as black chip and got the column to correctly change to player 1's color when circle button was clicked

--- app.js
+++ app.js
@@ -1,7 +1,72 @@
-// Work on print board
 
-$(document).ready(function() {});
+let game_on = true;
+let table = $("table tr");
+// player 1 Turn Mechanics
+let player1 = prompt("Player One: Enter your name, you will be Blue");
+let player1Color = 'rgb(0,0,0)';
+let activePlayer = 1;
+let activeName = player1;
+let activeColor = player1Color;
 
-// work on player turn
+$(document).ready(function() {
+    $("h3").text(
+      player1 + ": it is your move to pick a column to drop your black disc"
+    );
+    $(".board button").on("click", function() {
+      let col = $(this)
+        .closest("td")
+        .index();
+      let bottomOpen = reviewBottom(col);
+      switchColor(bottomOpen, col, activeColor);
+    });
+  });
 
-// work on
+// Player 2 Turn Mechanics
+// let player2 = prompt("Player Two: Enter your name, you will be Red");
+let player2Color = "rgb(237, 45, 73)";
+
+
+
+let showWin = (rowNum, colNum) => {
+  console.log("You won starting at this row,col");
+  console.log(rowNum);
+  console.log(colNum);
+};
+
+function switchColor(rowIndex, colIndex, color) {
+  return table
+    .eq(rowIndex)
+    .find("td")
+    .eq(colIndex)
+    .find("button")
+    .css("background-color", color);
+}
+
+function returnColor(rowIndex, colIndex) {
+  return table
+    .eq(rowIndex)
+    .find("td")
+    .eq(colIndex)
+    .find("button")
+    .css("background-color");
+}
+
+function reviewBottom(colIndex) {
+  let showColor = returnColor(5, colIndex);
+  for (let row = 5; row >= 0; row--) {
+    showColor = returnColor(row, colIndex);
+    if (showColor === 'rgb(255, 255, 255)') {
+      return row;
+    }
+  }
+}
+
+function reviewColorMatch(one, two, three, four) {
+  return (
+    one === two &&
+    one === three &&
+    one === four &&
+    one !== "rgb(255, 255, 255)" &&
+    one !== undefined
+  );
+}
