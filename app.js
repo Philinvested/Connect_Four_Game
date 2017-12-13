484f928626cec283d2d6b9d8955c806e1c1ae7f6
philinvested <philinvested@gmail.com>
2017-12-12 19:13:44 -0500

    Added the logic for the verticalWinCondition successfully

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-
 let table = $("table tr");
 
 // player set up
-let player1 = prompt("Player One: Input your name. Your assigned disc color will be Black");
+let player1 = prompt(
+  "Player One: Input your name. Your assigned disc color will be Black"
+);
 let player1Color = "rgb(0,0,0)";
-let player2 = prompt("Player Two: Input your name. Your assigned disc color will be will be Red");
-let player2Color = "rgb(237, 45, 73)";
+let player2 = prompt(
+  "Player Two: Input your name. Your assigned disc color will be will be Red"
+);
+let player2Color = "rgb(255, 50, 50)";
 
 let activePlayer = 1;
 let activeName = player1;
@@ -13,41 +16,54 @@ let activeColor = player1Color;
 
 // Game Mechanics
 $(document).ready(function() {
-    $("h3").text(player1 + ": it is your move to choose a column to drop your black discs");
-    $(".board button").on("click", function() {
-        let col = $(this)
-        .closest("td")
-        .index();
-        let bottomOpen = reviewBottom(col);
-        switchColor(bottomOpen, col, activeColor);
+  $("h3").text(
+    player1 + ": it is your move to choose a column to drop your black discs"
+  );
+  $(".board button").on("click", function() {
+    let col = $(this)
+      .closest("td")
+      .index();
+    let bottomOpen = reviewBottom(col);
+    switchColor(bottomOpen, col, activeColor);
 
-        if(horizontalWinCondition()){
-            gameEnd(activeName);
-        }
+    // calling winning conditions
+    if (
+      horizontalWinCondition() ||
+      verticalWinCondition() ||
+      diagonalWinCheck()
+    ) {
+      gameEnd(activeName);
+    }
 
-        activePlayer = activePlayer * -1;
+    activePlayer = activePlayer * -1;
 
-        if (activePlayer === 1) {
-            activeName = player1;
-            $("h3").text(activeName +": it is your move to choose a column to drop your black discs.");
-            activeColor = player1Color;
-        } else {
-            activeName = player2;
-            $("h3").text(activeName +": it is your move to choose a column to drop your red discs.");
-            activeColor = player2Color;
-        }
-    })
+    if (activePlayer === 1) {
+      activeName = player1;
+      $("h3").text(
+        activeName +
+          ": it is your move to choose a column to drop your black discs."
+      );
+      activeColor = player1Color;
+    } else {
+      activeName = player2;
+      $("h3").text(
+        activeName +
+          ": it is your move to choose a column to drop your red discs."
+      );
+      activeColor = player2Color;
+    }
+  });
 });
 
 let showWin = (rowNum, colNum) => {
-    console.log("You won starting at this row,col");
-    console.log(rowNum);
-    console.log(colNum);
-}
+  console.log("You won starting at this row,col");
+  console.log(rowNum);
+  console.log(colNum);
+};
 
 // Color Assignments
 function switchColor(rowIndex, colIndex, color) {
-    return table
+  return table
     .eq(rowIndex)
     .find("td")
     .eq(colIndex)
@@ -56,7 +72,7 @@ function switchColor(rowIndex, colIndex, color) {
 }
 
 function returnColor(rowIndex, colIndex) {
-    return table
+  return table
     .eq(rowIndex)
     .find("td")
     .eq(colIndex)
@@ -65,57 +81,89 @@ function returnColor(rowIndex, colIndex) {
 }
 
 function reviewBottom(colIndex) {
-    let showColor = returnColor (5, colIndex);
-    for (let row = 5; row >= 0; row--) {
-        showColor = returnColor(row, colIndex);
-        if (showColor === "rgb(255, 255, 255)") {
-            return row;
-        }
+  let showColor = returnColor(5, colIndex);
+  for (let row = 5; row >= 0; row--) {
+    showColor = returnColor(row, colIndex);
+    if (showColor === "rgb(255, 255, 255)") {
+      return row;
     }
+  }
 }
 
 function reviewColorMatch(one, two, three, four) {
-    return (
+  return (
     one === two &&
     one === three &&
     one === four &&
     one !== "rgb(255, 255, 255)" &&
     one !== undefined
-    );
+  );
 }
 
 function reviewColorMatch(one, two, three, four) {
-    return (
+  return (
     one === two &&
     one === three &&
     one === four &&
     one !== "rgb(255, 255, 255)" &&
     one !== undefined
-    );
+  );
 }
 
 // Win Conditions Logic
 function horizontalWinCondition() {
-    for (var row = 0; row <= 7 ; row++) {
-        for (var col = 0; col < 4; col++) {
-            if (reviewColorMatch(returnColor(row,col), returnColor(row,col+1) ,returnColor(row,col+2), returnColor(row,col+3))) {
-                console.log('horiz');
-                showWin(row,col);
-                return true;
-            } else {
-                continue;
-            }
-        }
+  for (let row = 0; row < 6; row++) {
+    for (let col = 0; col < 4; col++) {
+      if (
+        reviewColorMatch(
+          returnColor(row, col),
+          returnColor(row, col + 1),
+          returnColor(row, col + 2),
+          returnColor(row, col + 3)
+        )
+      ) {
+        console.log("horizontal");
+        showWin(row, col);
+        return true;
+      } else {
+        continue;
+      }
     }
+  }
 }
+
+function verticalWinCondition() {
+  for (let col = 0; col < 7; col++) {
+    for (let row = 0; row < 3; row++) {
+      if (
+        reviewColorMatch(
+          returnColor(row, col),
+          returnColor(row + 1, col),
+          returnColor(row + 2, col),
+          returnColor(row + 3, col)
+        )
+      ) {
+        console.log("vertical");
+        showWin(row, col);
+        return true;
+      } else {
+        continue;
+      }
+    }
+  }
+}
+
+
 
 // Game Ending Mechanic
 function gameEnd(winningPlayer) {
-    for (var col = 0; col < 7; col++) {
-        for (var row = 0; row < 7; row++) {
-            $('h3').fadeOut('fast');
-            $('h2').fadeOut('fast');
-            $('h1').text(winningPlayer+" has won! Refresh your browser to play again!").css("fontSize", "50px")
-        }
+  for (let col = 0; col < 7; col++) {
+    for (let row = 0; row < 7; row++) {
+      $("h3").fadeOut("fast");
+      $("h2").fadeOut("fast");
+      $("h1")
+        .text(winningPlayer + " has won! Refresh your browser to play again!")
+        .css("fontSize", "50px");
     }
+  }
 }
